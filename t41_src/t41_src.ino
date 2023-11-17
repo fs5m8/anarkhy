59dbf853af117a9f4dbb2e50283a89b920763a22
fs5m8 <fsco3092@protonmail.com>
2023-11-17 13:21:50 +0900

    feat: deserialize json

--- t41_src/t41_src.ino
+++ t41_src/t41_src.ino
@@ -1,28 +1,41 @@
-#include "SPI.h"
-#include "SD.h"
-#define 
+#include <SPI.h>
+#include <SD.h>
+#include <ArduinoJson.h>
 #define SENTENCE_LENGTH 10;
+
 int chipSelect = BUILTIN_SDCARD;
-size_t loadFile(char* fdp, char* path);
 char* content = (char*)calloc((size_t)4096, sizeof(char));
+DynamicJsonDocument jcontent(500);
+
+void loadFile(char* fdp, char* path);
 
 void setup() {
   Serial.begin(115200);
   while (!Serial);
   while (!SD.begin(chipSelect)) delay(2000);
+  
   if (content == NULL) {
     Serial.println("Failed to allocate memory for reading");
     return;
   }
-  size_t len = loadFile(content, "/test.json");
   
-  if (content != NULL) free(fdp);
+  loadFile(content, "/test.json");
+  DeserializationError deserError = deserializeJson(jcontent, content);
+  
+  if (deserError) {
+    Serial.print("Failed to deserialize JSON: ");
+    Serial.println(deserError.c_str());
+    if (content != NULL) free(content);
+    return;
+  }
+  
+  if (content != NULL) free(content);
 }
 
 void loop() {
 }
 
-size_t loadFile(char* content, char* path) {
+void loadFile(char* content, char* path) {
   File file;
   file = SD.open(path);
   if (!file) {
@@ -36,5 +49,5 @@ size_t loadFile(char* content, char* path) {
   }
   Serial.printf("%u bytes readed\n", len);
   file.close();
-  return len;
+  return;
 }
